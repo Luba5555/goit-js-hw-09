80c028a89ea4290d68977119082a6e169c20ac74
Luba5555 <113469906+Luba5555@users.noreply.github.com>
2023-03-25 21:33:49 +0000

    Deploying to gh-pages from @ Luba5555/goit-js-hw-09@2ed4a72c344654b81c2bc88ee260c973eed4d908 🚀

---
+++ 01-color-switcher.7c562075.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let a;e.addEventListener("click",(function(o){e.disabled=!0,a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),o.addEventListener("click",(()=>{e.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.7c562075.js.map
